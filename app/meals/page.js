46c80a7407679dda818afb6bc66bf69f966c5146
HibaMB <hibabanat.95@gmail.com>
2025-12-31 21:19:26 +0300

    handle loading message inside component using suspense/fallback (on specific api / fetching data), add error handling in case of error .. loading and error are reserved pages .. error is client component only

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,10 +2,18 @@ import Link from "next/link";
 import MealsGrid from "../components/meals/meals-grid";
 import classes from "./page.module.css";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
 
-export default async function MealsPage() {
+export async function Meals() {
   const meals = await getMeals();
+  return (
+    <>
+      <MealsGrid meals={meals} />
+    </>
+  );
+}
 
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -19,7 +27,9 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<p className={classes.loading}>Loading meals..</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
