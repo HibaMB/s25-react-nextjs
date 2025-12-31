70fcf803907c73eb0378d10cff89ec24e5258d62
HibaMB <hibabanat.95@gmail.com>
2025-12-31 20:27:21 +0300

    add mealitem and mealgrid components, import in meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,23 @@
+import Link from "next/link";
+import MealsGrid from "../components/meals/meals-grid";
+import classes from "./page.module.css";
+
 export default function MealsPage() {
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>MealsPage</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicous meals, created {""}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your fav. recipe and cook it yourself.</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your fav. recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
