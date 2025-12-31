eb5bc01242a82c3423d5efc43939344e9693d285
HibaMB <hibabanat.95@gmail.com>
2025-12-31 21:41:52 +0300

    add not-found page

---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+export default function NotFoundPage() {
+  return (
+    <>
+      <main className="not-found">
+        <h1>Not Found</h1>
+        <p>Unfortunatly, we could not found the requested page or resource!</p>
+      </main>
+    </>
+  );
+}
